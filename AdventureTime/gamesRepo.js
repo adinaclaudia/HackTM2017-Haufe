d9a5586b115a69de81b8d1edcafd6a0ad4fc3103
Boc Radu <radu.boc@haufe-lexware.com>
2017-05-27 12:55:32 +0300

    initial commit

---
+++ AdventureTime/gamesRepo.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "GAMES" : [
+        {
+            "The Dick":[
+                {"You see a dick. What do?":["Run"]},
+                {"Shit son. That was close":["Walk away"]}
+            ]
+        }
+    ]
+};
